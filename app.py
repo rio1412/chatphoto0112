6c4e4d34a895edf271147c8655c2d4497a74c83e
rio1412 <118584074+rio1412@users.noreply.github.com>
2023-04-30 22:12:56 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,102 +1,68 @@
-import streamlit as st
-import requests
-import base64
-from io import BytesIO
-import numpy as np
 import cv2
-import tensorflow as tf
-import tensorflow_hub as hub
+import streamlit as st
+import numpy as np
 
-st.title("ESRGAN: Super-Resolution")
+# 背景差分法を使用するための設定
+background_subtractor = cv2.createBackgroundSubtractorMOG2()
 
-# モデルの読み込み
-hub_url = 'https://tfhub.dev/captain-pool/esrgan-tf2/1'
-sr_model = hub.load(hub_url)
+# streamlitの設定
+st.set_page_config(page_title="Background Subtraction", page_icon=":camera:", layout="wide")
+st.title("Background Subtraction")
 
-# 画像のアップロード用ウィジェット
-uploader = st.file_uploader(label='Upload an image')
+# カメラ起動ボタンを作成する
+start_camera = st.button('カメラを起動する')
 
-# スライダーの作成
-contrast_slider = st.slider(
-    label='Contrast:',
-    min_value=0.0,
-    max_value=2.0,
-    step=0.01,
-    value=1.0
-)
+# カメラ画像を表示するエリアを作成する
+image_placeholder = st.empty()
+image_placeholder2 = st.empty()
 
-hue_slider = st.slider(
-    label='Hue:',
-    min_value=-0.5,
-    max_value=0.5,
-    step=0.01,
-    value=0.0
-)
+# カメラが起動しているかどうかのフラグ
+camera_running = False
 
-brightness_slider = st.slider(
-    label='Brightness:',
-    min_value=-0.5,
-    max_value=0.5,
-    step=0.01,
-    value=0.0
-)
+if start_camera:
+    # カメラを起動する
+    capture = cv2.VideoCapture(0)
+    camera_running = True
 
-white_balance_slider = st.slider(
-    label='White Balance:',
-    min_value=0.0,
-    max_value=2.0,
-    step=0.01,
-    value=1.0
-)
+while camera_running:
+    # フレームを読み込む
+    ret, frame = capture.read()
 
-# ボタンのクリック時の処理
-def on_button_click():
-    if uploader is None:
-        st.warning('Please select an image to enhance.')
-        return
-    input_image = cv2.imdecode(np.fromstring(uploader.read(), np.uint8), cv2.IMREAD_COLOR)
-    input_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2RGB)
+    # 背景差分法を適用する
+    foreground_mask = background_subtractor.apply(frame)
 
-    # コントラストの調整
-    contrast = contrast_slider
-    output_image = tf.image.adjust_contrast(input_image, contrast)
+    # ノイズを取り除く
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    foreground_mask = cv2.morphologyEx(foreground_mask, cv2.MORPH_OPEN, kernel)
 
-    # 色味の調整
-    hue = hue_slider
-    output_image = tf.image.adjust_hue(output_image, hue)
+    # 二値化する
+    threshold_value = 128
+    ret, foreground_mask = cv2.threshold(foreground_mask, threshold_value, 255, cv2.THRESH_BINARY)
 
-    output_image = tf.image.convert_image_dtype(output_image, tf.float32)
-    output_image = tf.expand_dims(output_image, axis=0)
-    output_image = sr_model(output_image)[0]
-    output_image = tf.squeeze(output_image)
-    output_image = tf.clip_by_value(output_image, 0, 1)
-    output_image = tf.image.convert_image_dtype(output_image, dtype=tf.uint8)
-    output_image = np.array(output_image)
-    output_image = cv2.cvtColor(output_image, cv2.COLOR_RGB2BGR)
-    output_image = cv2.cvtColor(output_image, cv2.COLOR_BGR2RGB)
+    # 輪郭を抽出する
+    contours, hierarchy = cv2.findContours(foreground_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    # 入力画像を表示
-    st.image(input_image, channels='RGB', caption='Input Image')
+    # 輪郭の周囲長を計算する
+    for contour in contours:
+      perimeter = cv2.arcLength(contour, True)
 
-    # 出力画像を表示
-    st.image(output_image, channels='RGB', caption='Enhanced Image')
+      # 輪郭の周囲長が一定以上であれば、人が動いているとみなす
+      if perimeter > 300:
+          cv2.drawContours(frame, [contour], -1, (0, 255, 0), 2)
 
-    # 画像の保存
-    file_extension = '.jpg'
-    with open('enhanced_image' + file_extension, 'wb') as f:
-        f.write(cv2.imencode(file_extension, output_image)[1])
+    # 結果を表示する
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    foreground_mask = cv2.cvtColor(foreground_mask, cv2.COLOR_GRAY2RGB)
+    image_placeholder.image(frame, channels="RGB", use_column_width=True)
+    image_placeholder2.image(foreground_mask, channels="RGB", use_column_width=True)
 
-# ボタンの作成
-if uploader is not None:
-    st.button(label='Enhance image', on_click=on_button_click)
+    # "q"キーで終了する
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-# ウィジェットの表示
-st.write('Adjust the image:')
-st.write('Contrast:')
-st.write(contrast_slider)
-st.write('Hue:')
-st.write(hue_slider)
-st.write('Brightness:')
-st.write(brightness_slider)
-st.write('White_balance:')
-st.write(white_balance_slider)
+# カメラを解放する
+if camera_running:
+    capture.release()
+
+# ウィンドウをすべて閉じる
+cv2.destroyAllWindows()
