2f5abfde5ad2854d3c770178bb35aea4dda4e269
rio1412 <118584074+rio1412@users.noreply.github.com>
2023-04-30 22:30:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,47 @@
 import streamlit as st
-import streamlit_webrtc as webrtc
+from streamlit_webrtc import VideoTransformer, AudioTransformer, webrtc_streamer, RTCConfiguration
+
+class VideoProcessor(VideoTransformer):
+    def __init__(self):
+        self.frame_count = 0
+
+    def transform(self, frame):
+        self.frame_count += 1
+        # ここでOpenCVなどのライブラリを使用してフレームを処理することができます。
+        # この例では、単純にフレームのカウントを表示しています。
+        st.write(f"Processed frame {self.frame_count}")
+        return frame
+
+class AudioProcessor(AudioTransformer):
+    def __init__(self):
+        self.audio_count = 0
+
+    def transform(self, audio_chunk):
+        self.audio_count += 1
+        # ここでPyAudioなどのライブラリを使用してオーディオを処理することができます。
+        # この例では、単純にオーディオのカウントを表示しています。
+        st.write(f"Processed audio chunk {self.audio_count}")
+        return audio_chunk
 
 def main():
     st.title("WebRTCを使用したストリームリットアプリ")
 
-    # カメラを起動する
-    video_stream = webrtc.VideoTransformer(
-        input_video=True,
-        preferred_output_format=webrtc.OutputFormat.MJPEG,
-        on_error=st.error,
+    rtc_configuration = RTCConfiguration(
+        {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
     )
 
-    # マイクを起動する
-    audio_stream = webrtc.AudioTransformer(
-        input_audio=True,
-        on_error=st.error,
-    )
+    video_transformer = VideoProcessor()
+    audio_transformer = AudioProcessor()
 
-    # WebRTCのメディアストリームを表示する
     webrtc_streamer(
         key="example",
-        video_transformer_factory=video_stream,
-        audio_transformer_factory=audio_stream,
+        video_transformer_factory=video_transformer,
+        audio_transformer_factory=audio_transformer,
+        rtc_configuration=rtc_configuration,
+        async_processing=True,
+        enable_video=True,
+        enable_audio=True,
+        client_settings={"width": 640, "height": 480},
     )
 
 if __name__ == "__main__":
