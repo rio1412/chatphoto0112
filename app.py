dd8297efb52a5c407b2b5185db797e519ee72312
rio1412 <118584074+rio1412@users.noreply.github.com>
2023-04-30 22:16:16 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,68 +1,31 @@
-import cv2
+%%writefile app.py 
+
 import streamlit as st
-import numpy as np
+import cv2
 
-# 背景差分法を使用するための設定
-background_subtractor = cv2.createBackgroundSubtractorMOG2()
+def main():
+    st.title("Webカメラを使用したストリームリットアプリ")
 
-# streamlitの設定
-st.set_page_config(page_title="Background Subtraction", page_icon=":camera:", layout="wide")
-st.title("Background Subtraction")
+    cap = cv2.VideoCapture(0)
 
-# カメラ起動ボタンを作成する
-start_camera = st.button('カメラを起動する')
+    if not cap.isOpened():
+        st.error("カメラを開けませんでした")
+        return
 
-# カメラ画像を表示するエリアを作成する
-image_placeholder = st.empty()
-image_placeholder2 = st.empty()
+    while True:
+        ret, frame = cap.read()
 
-# カメラが起動しているかどうかのフラグ
-camera_running = False
+        if not ret:
+            st.warning("フレームを取得できませんでした")
+            break
 
-if start_camera:
-    # カメラを起動する
-    capture = cv2.VideoCapture(0)
-    camera_running = True
+        # OpenCVでフレームを処理する
+        # ...
 
-while camera_running:
-    # フレームを読み込む
-    ret, frame = capture.read()
+        # ストリームリットアプリでフレームを表示する
+        st.image(frame, channels="BGR")
 
-    # 背景差分法を適用する
-    foreground_mask = background_subtractor.apply(frame)
+    cap.release()
 
-    # ノイズを取り除く
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-    foreground_mask = cv2.morphologyEx(foreground_mask, cv2.MORPH_OPEN, kernel)
-
-    # 二値化する
-    threshold_value = 128
-    ret, foreground_mask = cv2.threshold(foreground_mask, threshold_value, 255, cv2.THRESH_BINARY)
-
-    # 輪郭を抽出する
-    contours, hierarchy = cv2.findContours(foreground_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    # 輪郭の周囲長を計算する
-    for contour in contours:
-      perimeter = cv2.arcLength(contour, True)
-
-      # 輪郭の周囲長が一定以上であれば、人が動いているとみなす
-      if perimeter > 300:
-          cv2.drawContours(frame, [contour], -1, (0, 255, 0), 2)
-
-    # 結果を表示する
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    foreground_mask = cv2.cvtColor(foreground_mask, cv2.COLOR_GRAY2RGB)
-    image_placeholder.image(frame, channels="RGB", use_column_width=True)
-    image_placeholder2.image(foreground_mask, channels="RGB", use_column_width=True)
-
-    # "q"キーで終了する
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# カメラを解放する
-if camera_running:
-    capture.release()
-
-# ウィンドウをすべて閉じる
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+    main()
